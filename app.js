0679d97ff208b61c8ed38d2bcdc4e226b6133cd0
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-02 19:35:30 +0300

    project boilerplate

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//JOBS API
+require('express-async-errors')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to Jobs API page')
+})
+
+const port = 4000 || process.env.PORT
+
+const start = async ()=>{
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, ()=>{
+        console.log(`Server is listening on port ${port}`)
+    })
+}
+
+start()
