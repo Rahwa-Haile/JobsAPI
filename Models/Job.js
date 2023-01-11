bdd7f4ccff618b5fc63bc0bccb1f7ec73e1276d8
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-11 10:55:20 +0300

    created a model for the jobs

---
+++ Models/Job.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide company name'],
+        maxlength: 50
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide position name'],
+        maxlength: 100
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'interview', 'declined'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'please provide a user']
+    },
+}, {timestamp: true})
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -9,7 +9,7 @@ const authMiddleware = async (req, res, next)=>{
  }
 
  const token = authHeader.split(' ')[1]
-
+ 
  try{
     const payload = await jwt.verify(token, process.env.JWT_SECRET)
     //attach user to the job routes
