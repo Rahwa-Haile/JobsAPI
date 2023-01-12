9e0a5e8d69c850fbdc963880ab224775bf3ffa21
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-12 07:10:40 +0300

    Setup the functionality for getting and updating a job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,6 @@
 const {StatusCodes} = require('http-status-codes')
 const Job = require('../models/Job')
+const { NotFoundError, BadRequestError } = require('../errors')
 
 const getAllJobs = async (req, res)=>{
     const {userId} = req.user
@@ -12,11 +13,25 @@ const createJob = async (req, res)=>{
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({ job })
 }
-const getJob = (req, res)=>{
-    res.send('get a job')
+const getJob = async (req, res)=>{
+    const {user: {userId}, params: {id: jobId}} = req
+    const job = await Job.findOne({ _id: jobId, createdBy: userId })
+    if(!job){
+        throw new NotFoundError(`No such job with an id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
-const updateJob = (req, res)=>{
-    res.send('update a job')
+const updateJob = async (req, res)=>{
+    const { body: { company, position}, user: {userId}, params: {id: jobId}} = req
+
+    if(!company || !position){
+        throw new BadRequestError('company name and position must be provided')
+    }
+    const job = await Job.findOneAndUpdate({ createdBy: userId , _id: jobId }, req.body, {new: true, runValidators: true})
+    if(!job){
+        throw new NotFoundError(`No such job with an id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = (req, res)=>{
     res.send('delete a job')
